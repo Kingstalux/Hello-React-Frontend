4ebb11bbc4ebba2f205b25c73aae300b84c5faf1
Kingstalux <kingsleyngu25@gmail.com>
2022-05-14 12:14:14 +0100

    Add greetings reducer

---
+++ src/redux/greetings/greetings.js
@@ -0,0 +1,78 @@
+const FETCH_GREETINGS_BEGIN = 'hello-react-frontend/greetings/FETCH_GREETINGS_BEGIN';
+const FETCH_GREETINGS_SUCCESS = 'hello-react-frontend/greetings/FETCH_GREETINGS_SUCCESS';
+const FETCH_GREETINGS_FAILURE = 'hello-react-frontend/greetings/FETCH_GREETINGS_FAILURE';
+
+const initialState = {
+  data: [],
+  loading: false,
+  error: null,
+};
+
+const fetchGreetingsBegin = () => ({
+  type: FETCH_GREETINGS_BEGIN,
+});
+
+const fetchGreetingsSuccess = (payload) => ({
+  type: FETCH_GREETINGS_SUCCESS,
+  payload,
+});
+
+const fetchGreetingsFailure = (error) => ({
+  type: FETCH_GREETINGS_FAILURE,
+  payload: { error },
+});
+
+function handleErrors(response) {
+  if (!response.ok) {
+    throw Error(alert('Congrats you broke the app'));
+  }
+  return response;
+}
+
+const url = 'http://localhost:3000/v1/greetings';
+
+export function fetchGreetings() {
+  return (dispatch) => {
+    dispatch(fetchGreetingsBegin());
+    return fetch(url)
+      .then(handleErrors)
+      .then((res) => res.json())
+      .then((json) => {
+        const data = [];
+        data.push(json);
+        dispatch(fetchGreetingsSuccess(data));
+      })
+      .catch((error) => dispatch(fetchGreetingsFailure(error)));
+  };
+}
+
+const greetingsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_GREETINGS_BEGIN:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+
+    case FETCH_GREETINGS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        data: action.payload,
+      };
+
+    case FETCH_GREETINGS_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.error,
+        data: [],
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default greetingsReducer;
